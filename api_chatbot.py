1b2ff4079ad47cbad3ac3fc0d0969ab696561141
KishlayAmbastha14 <kishlaykumar087@gmail.com>
2025-11-22 20:16:11 +0530

    added new versions

--- api_chatbot.py
+++ api_chatbot.py
@@ -9,7 +9,9 @@ from langchain_community.vectorstores import FAISS
 # from langchain.chains import create_retrieval_chain
 # from langchain.chains import RetrievalQA
 from langchain_groq import ChatGroq
-from langchain.memory.buffer import ConversationBufferMemory
+# from langchain.memory.buffer import ConversationBufferMemory
+from langchain_core.memory import ConversationBufferMemory
+
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain.chains import ConversationalRetrievalChain
 from langchain.chains import ConversationalRetrievalChain
